cb70e073897cabea2bacf0cb7ff81c7c28e9a7d6
hiranoyusuke <user@hiranoyusuke.local>
2019-09-12 23:27:48 +0900

    jQuery合計金額計算

--- src/main/resources/static/js/countPrice.js
+++ src/main/resources/static/js/countPrice.js
@@ -1,23 +1,40 @@
 
 $(function(){
+	calc_price();
 	
-	$('.radio-inline').on('click', function(){
+	$('.size').on('change', function(){
+		calc_price();
+	});
+	
+	$('.toppings').on('click', function(){
+		calc_price();
+	});
+	
+	$('#pizaCount').on('change',function(){
+		calc_price();
+	});
+	
+	
+});
+	
+	
+	function calc_price(){
 		var size = $('input[name=size]:checked').val();
 		var topping_count = $('.toppings input:checkbox:checked').length;
 		var piza_count = $("#pizaCount").val();
-		console.log(size);
+		console.log(topping_count);
 		
 		if(size == 'm'){
-			var sizePrice = $(".priceM").text();
-			console.log(sizePrice);
+			var sizePrice = parseInt($(".priceM").text().split(',').join('').trim());
 			var toppingPrice = topping_count * 200;
 		} else {
-			sizePrice = $(".priceL").text();
-			var toppingPrice = topping_count * 200;
+			sizePrice = parseInt($(".priceL").text().split(',').join('').trim());
+			var toppingPrice = topping_count * 300;
 		}
+		
 		
 		var allPrice = (sizePrice + toppingPrice) * piza_count;
 		$("#total-price").html('<input type="hidden" name="totalPrice">' + allPrice.toLocaleString());
 		
-	});
-});
+	};
+	
