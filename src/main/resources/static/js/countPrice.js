22c2b5dab5a8dde138bed9cf7522dd3f8080faa2
yusuke39 <hiranoyusuke39@gmail.com>
2019-09-12 21:05:01 +0900

    jquery商品金額計算

--- src/main/resources/static/js/countPrice.js
+++ src/main/resources/static/js/countPrice.js
@@ -1,6 +1,23 @@
 
 $(function(){
 	
-	console.log('ありがとう');
-	
+	$('.radio-inline').on('click', function(){
+		var size = $('input[name=size]:checked').val();
+		var topping_count = $('.toppings input:checkbox:checked').length;
+		var piza_count = $("#pizaCount").val();
+		console.log(size);
+		
+		if(size == 'm'){
+			var sizePrice = $(".priceM").text();
+			console.log(sizePrice);
+			var toppingPrice = topping_count * 200;
+		} else {
+			sizePrice = $(".priceL").text();
+			var toppingPrice = topping_count * 200;
+		}
+		
+		var allPrice = (sizePrice + toppingPrice) * piza_count;
+		$("#total-price").html('<input type="hidden" name="totalPrice">' + allPrice.toLocaleString());
+		
+	});
 });
