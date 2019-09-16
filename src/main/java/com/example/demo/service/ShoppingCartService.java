3d98bbcd8674d6add067402ea7f44e483f54be23
yusuke39 <hiranoyusuke39@gmail.com>
2019-09-16 17:56:21 +0900

    order_toppingsにインサートする部分をメソッド化しました

--- src/main/java/com/example/demo/service/ShoppingCartService.java
+++ src/main/java/com/example/demo/service/ShoppingCartService.java
@@ -18,18 +18,16 @@ import com.example.demo.repository.OrderToppingRepository;
 @Service
 @Transactional
 public class ShoppingCartService {
-	
-	
+
 	@Autowired
 	private OrderRepository orderRepository;
-	
+
 	@Autowired
 	private OrderItemRepository orderItemRepository;
-	
+
 	@Autowired
 	private OrderToppingRepository orderToppingRepository;
-	
-	
+
 	/**
 	 * order,order_item,order_toppingにアイテムをインサートをする.
 	 * 
@@ -37,64 +35,57 @@ public class ShoppingCartService {
 	 * @param userId
 	 */
 	public void addShopingCart(ShoppingCartForm form, Integer userId) {
-		//formから受け取った値をドメインに詰める
+		// formから受け取った値をドメインに詰める
 		Order order = new Order();
 		order.setUserId(userId);
 		order.setStatus(0);
 		order.setTotalPrice(0);
-		
+
 		OrderItem orderItem = new OrderItem();
 		orderItem.setItemId(Integer.parseInt(form.getItemId()));
 		orderItem.setSize(form.getSize());
 		orderItem.setQuantity(Integer.parseInt(form.getQuantity()));
 
 		List<Order> orderList = orderRepository.findOrderByUserIdAndStatus(userId, 0);
-		
-		if(orderList.size() ==  0) {
-			
+
+		if (orderList.size() == 0) {
+
 			Order orderDomain = orderRepository.insert(order);
 			orderItem.setOrderId(orderDomain.getId());
-			
-			OrderItem orderItemDomain = orderItemRepository.insert(orderItem);
-			
-			List<OrderTopping> orderToppingList =  new ArrayList<>();
-			OrderTopping orderTopping = new OrderTopping();
-			if(form.getOrderToppingList() != null) {
-				for(Integer toppingId : form.getOrderToppingList()) {
-					orderTopping.setToppingId(toppingId);
-					orderToppingList.add(orderTopping);
-					orderTopping.setOrderItemId(orderItemDomain.getId());
-					orderToppingRepository.insert(orderTopping);
-				}
-			}
 
-//			orderTopping.setOrderItemId(orderItemDomain.getId());
-//			
-//			orderToppingRepository.insert(orderTopping);
-			
+			OrderItem orderItemDomain = orderItemRepository.insert(orderItem);
+
+			insertOrderTopping(form, orderItemDomain);
+
 		} else {
-			
+
 			Order orderDomain = orderList.get(0);
-			
+
 			orderItem.setOrderId(orderDomain.getId());
 			OrderItem orderItemDomain = orderItemRepository.insert(orderItem);
-			
-			List<OrderTopping> orderToppingList =  new ArrayList<>();
-			OrderTopping orderTopping = new OrderTopping();
-			if(form.getOrderToppingList() != null) {
-				for(Integer toppingId : form.getOrderToppingList()) {
-					orderTopping.setToppingId(toppingId);
-					orderToppingList.add(orderTopping);
-					orderTopping.setOrderItemId(orderItemDomain.getId());
-					orderToppingRepository.insert(orderTopping);
-				}
-			}
-			
+
+			insertOrderTopping(form, orderItemDomain);
 		}
-		
-		
+
 	}
 
-	
-	
+	/**
+	 * オーダートッピングにインサートする部分をメソッド化.
+	 * 
+	 * @param form
+	 * @param orderItemDomain
+	 */
+	public void insertOrderTopping(ShoppingCartForm form, OrderItem orderItemDomain) {
+		List<OrderTopping> orderToppingList = new ArrayList<>();
+		OrderTopping orderTopping = new OrderTopping();
+		if (form.getOrderToppingList() != null) {
+			for (Integer toppingId : form.getOrderToppingList()) {
+				orderTopping.setToppingId(toppingId);
+				orderToppingList.add(orderTopping);
+				orderTopping.setOrderItemId(orderItemDomain.getId());
+				orderToppingRepository.insert(orderTopping);
+			}
+		}
+	}
+
 }
